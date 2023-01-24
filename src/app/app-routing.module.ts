cab357954ce84635c4ffcb815b6e649906fbfa1c
quebeen <kejatori@gmail.com>
2023-01-25 01:56:24 +0800

    setup navigtation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './history/history.component';
+import { HomeComponent } from './home/home.component';
+import { RecordsComponent } from './records/records.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent, pathMatch: "full"},
+  {path: "records", component: RecordsComponent, },
+  {path: "history", component: HistoryComponent},
+  {path: "settings", component: SettingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { RecordsComponent } from './records/records.component';
+import { HistoryComponent } from './history/history.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    RecordsComponent,
+    HistoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +28,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
